c8c707b2d143844ed6f3079ad569d512ff22c842
Grahulprdxn <rahulg@prdxn.com>
2024-03-25 19:44:09 +0530

    created html template and returned the template response views.py

--- my_tennis_club/members/urls.py
+++ my_tennis_club/members/urls.py
@@ -2,5 +2,5 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('members/', views.tennis_members, name="members"),
+    path('members/', views.tennis_members, name="members list"),
 ]
